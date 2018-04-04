eacff86f125609217ed170185b417a7891b53093
NicoAiko <nicochan95.anime@gmail.com>
2018-04-04 19:11:10 +0200

    Enable Build on all CPU types (x86 and x64 / 32- and 64-bit)

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -33,7 +33,7 @@ export default new Vuex.Store({
   plugins,
   state: {
     isReady: false,
-    currentAppVersion: '0.5.1',
+    currentAppVersion: '0.5.2',
   },
   mutations: {
     hydrateState(state, payload) {

110b91fd98c1e9353f2d4cc1832090c40a83613d
NicoAiko <nicochan95.anime@gmail.com>
2018-04-07 15:54:51 +0200

    Add AppIcon Update to v0.5.3

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -33,7 +33,7 @@ export default new Vuex.Store({
   plugins,
   state: {
     isReady: false,
-    currentAppVersion: '0.5.2',
+    currentAppVersion: '0.5.3',
   },
   mutations: {
     hydrateState(state, payload) {

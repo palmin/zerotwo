2504dc9ab545d99819e004074c452f2ee6809940
Nico Linnemann <n.linnemann@nicoaiko.de>
2019-08-27 09:09:37 +0200

    Add Sorting and Filter for Genres

--- src/components/NavigationToolbars/Items/Sort.vue
+++ src/components/NavigationToolbars/Items/Sort.vue
@@ -19,31 +19,194 @@
         <span>{{ $t('system.actions.sort') }}</span>
       </v-tooltip>
     </template>
-    <v-card>
-      <v-container fluid>
-        <v-flex
-          xs12
-          d-flex
-        >
-          <v-select
-            :items="['Name']"
-            label="Sort by"
-          />
-        </v-flex>
+    <v-card width="400">
+      <v-card-title>{{ $t('system.actions.sort') }}</v-card-title>
+      <v-container fluid grid-list-md>
+        <v-layout>
+          <v-flex xs8>
+            <v-select
+              v-model="sortItem"
+              :items="sortingItems"
+              :label="$t('system.sorting.sortingLabel')"
+              @change="changeSorting"
+            />
+          </v-flex>
+          <v-flex xs4>
+            <v-radio-group v-model="direction" column @change="changeDirection">
+              <v-radio :label="$t('system.sorting.directions.ascending')" value="asc" />
+              <v-radio :label="$t('system.sorting.directions.descending')" value="desc" />
+            </v-radio-group>
+          </v-flex>
+        </v-layout>
       </v-container>
+
+      <v-divider class="ma-4" />
+
+      <v-container fluid grid-list-md>
+        <v-layout>
+          <v-flex xs12>
+            <v-combobox
+              v-model="genreValues"
+              :items="genres"
+              clearable
+              :search-input.sync="genreSearch"
+              hide-selected
+              :label="$t('system.search.genres.label')"
+              :hint="$t('system.search.genres.hint')"
+              multiple
+              persistent-hint
+              small-chips
+              dense
+              @change="changeGenres"
+            >
+              <template v-slot:no-data>
+                <v-list-item>
+                  <v-list-item-content>
+                    <!-- eslint-disable-next-line vue/no-v-html -->
+                    <v-list-item-title v-html="$t('system.search.genres.noMatch', [genreSearch])" />
+                  </v-list-item-content>
+                </v-list-item>
+              </template>
+            </v-combobox>
+          </v-flex>
+        </v-layout>
+      </v-container>
+
+      <v-card-actions>
+        <v-btn text block @click="resetFilters">
+          <v-icon left>
+            mdi-restore
+          </v-icon>
+          {{ $t('system.actions.reset') }}
+        </v-btn>
+      </v-card-actions>
     </v-card>
   </v-menu>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Watch } from 'vue-property-decorator';
+import EventBus from '@/eventBus';
+
+interface SelectItem {
+  text: string;
+  value: string;
+}
 
 @Component
 export default class Sort extends Vue {
+  private genres = [
+    'Action',
+    'Mystery',
+  ];
+
+  private genreValues = [];
+
+  private genreSearch = null;
+
   private sortMenu: boolean = false;
+
+  private direction: string = 'asc';
+
+  private sortItem: string = 'title';
+
+  private created() {
+    EventBus.$on('resetAllSorts', () => {
+      this.resetFilters(false);
+    });
+  }
 
   private get isSortingPage(): boolean {
     return this.$route.meta && this.$route.meta.sortingPage;
+  }
+
+  private get sortingItems(): SelectItem[] {
+    const items: SelectItem[] = [];
+
+    switch (this.$route.name) {
+      case 'Watching':
+      case 'Repeating':
+      case 'Completed':
+      case 'Dropped':
+      case 'Planning':
+      case 'Paused':
+        items.push({
+          text: this.$t('system.sorting.title') as string,
+          value: 'title',
+        }, {
+          text: this.$t('system.sorting.score') as string,
+          value: 'score',
+        }, {
+          text: this.$t('system.sorting.progress') as string,
+          value: 'progressPercentage',
+        }, {
+          text: this.$t('system.sorting.startingDate') as string,
+          value: 'startDate',
+        }, {
+          text: this.$t('system.sorting.episodeAmount') as string,
+          value: 'episodeAmount',
+        });
+        break;
+      case 'SeasonPreview':
+        items.push({
+          text: this.$t('system.sorting.title') as string,
+          value: 'title',
+        }, {
+          text: this.$t('system.sorting.startingDate') as string,
+          value: 'startDateTimestamp',
+        }, {
+          text: this.$t('system.sorting.episodeAmount') as string,
+          value: 'episodes',
+        });
+        break;
+      default:
+        break;
+    }
+
+    return items;
+  }
+
+  private changeSorting(value: any) {
+    EventBus.$emit('changeSorting', {
+      sortBy: value,
+      direction: this.direction,
+    });
+  }
+
+  private changeDirection(value: string) {
+    if (!this.sortItem) {
+      this.sortItem = this.sortingItems[0].value;
+    }
+
+    EventBus.$emit('changeSorting', {
+      sortBy: this.sortItem,
+      direction: value,
+    });
+  }
+
+  private changeGenres(values: string[]) {
+    EventBus.$emit('changeFiltering', {
+      genres: values,
+    });
+  }
+
+  private resetFilters(emitEvents: boolean = true): void {
+    this.genreValues = [];
+    this.sortItem = 'title';
+    this.direction = 'asc';
+
+    if (emitEvents) {
+      this.changeGenres(this.genreValues);
+      this.changeSorting(this.sortItem);
+      // Direction is automatically send with sorting
+    }
+  }
+
+  @Watch('genreValues')
+  private onGenreValuesChange(value: string[]) {
+    if (value.length > 3) {
+      this.$nextTick(() => this.genreValues.pop());
+    }
   }
 }
 </script>

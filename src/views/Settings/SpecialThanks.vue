91c7e52571c504177635a8e6148bad6d45229a28
NicoAiko <n.linnemann@nicoaiko.de>
2019-08-28 09:23:56 +0200

    Add special thanks and supporters
    Add some translations
    Use a more readable Link for ko-fi
    Add pointer cursor for Ko-fi button

---
+++ src/views/Settings/SpecialThanks.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-tab-item :key="tabKey">
+    <v-card flat>
+      <v-layout class="px-1 py-2">
+        <v-flex xs6>
+          <div class="display-1">
+            {{ $t('pages.settings.specialThanks') }}
+          </div>
+
+          <v-divider />
+
+          <v-layout class="px-1 py-2">
+            <template v-for="(item, index) in getSpecialThanks">
+              <v-flex :key="`thanks-icon-${index}`" xs1>
+                <v-icon>mdi-{{ item.icon }}</v-icon>
+              </v-flex>
+
+              <v-flex :key="`thanks-name-${index}`" xs4>
+                {{ item.name }}
+              </v-flex>
+
+              <v-flex :key="`thanks-message-${index}`" xs7>
+                {{ item.message[currentLanguage] || item.message.en }}
+              </v-flex>
+            </template>
+          </v-layout>
+        </v-flex>
+
+        <v-flex xs6>
+          <div class="display-1">
+            {{ $t('pages.settings.supporters') }}
+          </div>
+
+          <v-divider />
+
+          <v-layout class="px-1 py-2">
+            <template v-for="(item, index) in getSupporters">
+              <v-flex :key="`support-icon-${index}`" xs1>
+                <v-icon>mdi-{{ item.icon }}</v-icon>
+              </v-flex>
+
+              <v-flex :key="`support-name-${index}`" xs4>
+                {{ item.name }}
+              </v-flex>
+
+              <v-flex :key="`support-message-${index}`" xs7>
+                {{ item.message[currentLanguage] || item.message.en }}
+              </v-flex>
+            </template>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+
+      <v-card-actions>
+        <v-layout justify-end>
+          <v-flex xs6>
+            <v-img
+              class="pointer-on-hover"
+              height="50"
+              contain
+              :src="require('@/assets/logos/Ko-fi-Support-Button.png')"
+              @click="OpenKofiPage"
+            />
+          </v-flex>
+        </v-layout>
+      </v-card-actions>
+    </v-card>
+  </v-tab-item>
+</template>
+
+<script lang="ts">
+import { shell } from 'electron';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import specialThanks from '@/assets/support/specialThanks.json';
+import supporters from '@/assets/support/supporters.json';
+
+@Component
+export default class SpecialThanksSettings extends Vue {
+  @Prop(String)
+  private tabKey!: string;
+
+  private get currentLanguage(): string {
+    return this.$i18n.locale;
+  }
+
+  private get getSpecialThanks() {
+    console.log(specialThanks);
+    return specialThanks;
+  }
+
+  private get getSupporters() {
+    return supporters;
+  }
+
+  private OpenKofiPage(): void {
+    shell.openExternal('https://ko-fi.com/nicoaiko');
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.pointer-on-hover:hover {
+  cursor: pointer;
+}
+</style>
